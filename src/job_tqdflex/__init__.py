ac0026d4182c1864749ea03ab9627a441631df1a
David-Araripe <david.araripe17@gmail.com>
2025-10-23 12:53:27 +0200

    add setuptools for dynamic versioning

--- src/job_tqdflex/__init__.py
+++ src/job_tqdflex/__init__.py
@@ -2,6 +2,7 @@
 """Joblib-tqdm: Parallel processing with progress bars using joblib and tqdm."""
 
 from .parallel import ParallelApplier, tqdm_joblib
+from .version_helper import get_version
 
-__version__ = "0.1.0"
+__version__ = get_version()
 __all__ = ["ParallelApplier", "tqdm_joblib"]
